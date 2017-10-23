b19850dc738497ea8a1e5fb4b8991f7a85f01e16
System Administrator <root@Admins-MacBook-Air.local>
2017-10-24 03:23:38 +0700

    first commit

--- app.js
+++ app.js
@@ -54,9 +54,64 @@ const SERVER_URL = (process.env.SERVER_URL) ?
     (process.env.SERVER_URL) :
     config.get('serverURL');
 
-const ApiURL = (process.env.ApiURL) ?
-    (process.env.ApiURL) :
-    config.get('apiURL');
+const FIRE_BASE_ADMIN = {
+    jobochat: {
+        databaseURL: "https://jobo-chat.firebaseio.com",
+        cert: {
+            "type": "service_account",
+            "project_id": "jobo-chat",
+            "private_key_id": "dadaa2894385e39becf4224109fd59ba866414f4",
+            "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDZDEwnCY6YboXU\nd0fSmOAL8QuPVNj6P+fJc+sa7/HUqpcZrnubJAfPYjDCiUOf9p6mo2g5nQEZiiim\nQYiB+KMt8sHPvRtNF5tWeXN3s7quKAJcwCZC8RySeiR9EfKTniI6QrFwQt0pU1Ay\ncPg/whb1LwXoyA6C7PErOEJ+xsDQmCxEOLmGrbmDe81tBJZIBU8WupV7j9416qOs\n3iPnYIJxr6gqJWKNp6ALUM/48c1pAompn6aB7zOweyvvfC6ZKuMUfsEii5FDYR+A\n9eeeghZFXv9VLp4zpsWUZqytGEEW9xgWdC5aCbMN6PoAvhbrr+CEz2hqimMFEqyn\nfRnrDTx3AgMBAAECggEAEGqys90wMO1jJ//hqdcwUxbnVe8H/l2pDX68EKyHcRt6\nFFIzPTfLc28s2voA6G+B7n67mmf6tlDR5Elept4Ekawj5q+aCgm4ESFcj3hDrXqP\nOy65diTAkX+1lNQvseSrGBcFTsVv7vlDPp122XO3wtHMs5+2IUcEss0tkmM8IErO\nmuG1TweQccK6CU+GdvtZ0bsMv16S0fBz9hNfWQ0JRtiBSMeYJahf1wMKoLPHzdfU\nMyK39U3JPHOjaQaYkj80MAdXVOT4fjy7j//p7cLT57Exj4y8jHFpwI9XRawCyKrw\nl6yLzHpGQ4To5ERur8JUtMHF9gYctDr3XI5zZ1fZ0QKBgQDxoZQtlxWpfHBPXwB3\nwclUqfsTZHvmCBeGROX73+Hy2S84W0lrvmr3mrLMnl6syx8OS4tZdA3s8pbvj0HH\nFD8IXV2acc3Mf+OfQiawRowobSSeSPUr//vsPYfobsMtLzOjiO0n20p/nVV3gGCG\nZQyUDuHZVDvSBGz3bUXDeHiZLwKBgQDl9HuIBkW3pcpGvfBMqwOyRhLJFEXL14Nh\npwJ2nBs7eTd09S95+P14s2Y0U2AGc96FmElVrXk8teSn982pocAW3mdD6KgBpC6m\nlEGCJB9da7f27qspUpqsne1+a4GfhBrFp3IVx9HOYgDsJ/xSLnr+Ajhn5lNiJMN5\n3H3iuUSvOQKBgQDi3W4ej+gKxYc9PllWF2BMWXwe7Q1XIOnVawLzxXSDal7nbu40\ndwg/icOuUlNZsSxrY4pmZoxcmDgWnE6J9/xmgiLMS2WKR9kTQizI/LPDkRX8d0ua\nEDIb0Hm2RaiC1/qH5Jul/EKqJrKEDMiT5nQ03vQ19Nxlhzo35STHLmksiQKBgQCQ\nEES8CUHwNfutqh07yv/71g66zuqTNCdpLFpMuKwO7Hgj29+siKMz1SC4s2s7X6gP\nBkMbXBzSPhpMaOD93woayabkUoO+038ueT85KyxDONL97rRopQmmDyLUysFgkEC9\nh5PftVnp9Fgjm0Fmsxv2uqlf3lpq6CFW3R44xl0TcQKBgHC+jSs3fVr7/0uTVXIE\n89V+ypBbPfI4T2Fl9wPuizTxmLTbbnq3neIVurs6RyM5bWUSPIIoU59NajgCBATL\naE8us6ldgDneXCDGt8z1YwFtpLz5H9ItkOMFl4+Y3WLbk3mgdvpI5M8YsgcnDQ8y\nk1GnVuyRg5oTiYM6g7UTvLnx\n-----END PRIVATE KEY-----\n",
+            "client_email": "firebase-adminsdk-h83yt@jobo-chat.iam.gserviceaccount.com",
+            "client_id": "117827674445250600196",
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://accounts.google.com/o/oauth2/token",
+            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-h83yt%40jobo-chat.iam.gserviceaccount.com"
+        }
+    },
+    production: {
+        databaseURL: "https://jobo-b8204.firebaseio.com",
+        cert: {
+            "type": "service_account",
+            "project_id": "jobo-b8204",
+            "private_key_id": "14ea0b26388024fd4e0aef26837d779e6360f70f",
+            "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC6hhT4dkFvQ9dx\n+LtdyCt69WV+ffL4d0qsFUaAZftHt4npIlyqKNImSWvtOyDYHFpwSosL99+Va1/G\n6EeKKvJgdH8iCEApaxCyCRM1oZuXNVfDc3sH39NJoTpilcNmEbDteTOUN1blpqry\nnIG476P7NxXanly/ltrJwP2iLn4fQHGrtXohEsx3eChPL1fMsOxA6YfXhPQlGrUz\nG0wSvxE9iz8T2PKQJrzXxzcKCYAD9lFViHYEdNMnv6T3MdVVthAVD5v5d092Mlah\nxqNmBpfaqVWpYnlGrrEH0czxip0ZVvGuAU6gvvfxOwhrtmwCdpJaQzhm4FaWlgrH\n94oCWerTAgMBAAECggEAQ28FNtyeBIlc4y3/I0UifxYoBuanCHAsVXFtpy73fTKc\nT+Zl5PjUHRZvR/mYArmhcrZodb+8HAuROVqxvoCPVxLXAalE9RRpmUwRn1KZaz3U\nSGvAH5UqkJSTBKBLX+PmeLxYSu4E4wryA7tUZNVyjfiY1IxrULLLz6QPrmorm8Uz\nvrb/NpNO7j1YiCVVx6cQH0/PA/hQwlLFW6XL9+X7ATUuxUQI4sjwGjA9we9bfU3M\nNvcovUsMwLEPg3TaHVnaeDbpf8X0GHvUMgkpDmNrFQkwskKWCtsIMWB35f5Rh50B\nRpzEF8RDlv98i8GQeFCM/sWuI4pE8mAOQi+gvxAxpQKBgQD6OIAVTh5xYFlvk3+O\nSOM/CcqrM5Gatg36cOQ2W8HvWz6cEKjiueWmfPGxq/pfLOQzA3MMRWlH+UDC7nME\nq3gvGoWaja4dqlbpWt343icaKqeViuybB/y80fsuhLWATrN8bggq3OplCI+Jg5Bw\n75x8zE8Ib2XIbwx5Ok+gqzXERQKBgQC+1PSkcOhQYIHy1zUiMbS7Klxq95Mzodjz\noTt4YtvjMuJCguJ2Eo6Rlf3ArtxFh+3TTncnttM1LezNRdjdRZdhjwX1qH4LT5Jx\n5b6Cw4JycLy9GB7VWnIx9xw2yvBKk7ZyyQCzZA3YcHpngbl3mpyzGryPgoZX4vMN\njOETAEXANwKBgQCzpg8nvL+UrRVpS2AAewpU/yW4hzzZ9C3TCmx/Lp/txvgLutZW\nehuMzhYFdzE6VhO9IJPgUpGFMEqz6dlAmA+g2gzkayaAfAUMY8YM4Qr3+Xn6nxTD\nNhfaRXRu8K8TYO3yv1kz1Qqg4WWU2JXCz/XtkA6KQtiz8C7ndtsmwuXGdQKBgGT1\nZThaQ43CgP1ovcOJaIRctOgics4uIglCk6PtKUfZ87ocZJLy3lpHcCgwWniuoTPZ\nn1BzeOn5kf5HpaPq3VvPvudobMavIlr/oPqtVKYW3sNrr2RQpXmpslOKqfXKkAvK\nK4S8ulZ3q0p3ZxfPxHc8/eUuuMRmXRAeKDVVP5GhAoGAW+7NYzQpN5LTVh4XD7yR\nqPSwvYu8srmGB+spp8GO+1VJGYqNI9V35jTkbnZk3kJlYli72npBr96wnxUK/ln2\nOm50rCs+7AkbvzPGkmtMzcOCpstrs2GqtQz8UQGMpsMrlZ7g6lKG42r7DpQ8G/vj\n3Hg+Lu6M8x26b5mFimstO0Y=\n-----END PRIVATE KEY-----\n",
+            "client_email": "firebase-adminsdk-q7ytj@jobo-b8204.iam.gserviceaccount.com",
+            "client_id": "113764809503712074592",
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://accounts.google.com/o/oauth2/token",
+            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-q7ytj%40jobo-b8204.iam.gserviceaccount.com"
+        }
+    },
+    Jobo: {
+        databaseURL: "https://jobfast-359da.firebaseio.com",
+        cert: {
+            "type": "service_account",
+            "project_id": "jobfast-359da",
+            "private_key_id": "faf771e474a27ed686df35ef37eba42836d1952e",
+            "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCq+hHcEgWbqtAM\nzpOBNCIedvSDHkrBqFh/+YjCdPda74C5fBOzbBr4n1Jnl0KQBCdF3+RU0f8psSf3\nIt8ByaPNzEnnU0/21Mbo6UewNh/01lrmIq8yC0LvbrCTsR4A6Kyz45yx0GiI9l+a\nZ17MyoaoiY/n0kr752PXGHdDVhH+n+v8zVCCwk0jyr4Ibsb8AThD2wXbspUn7yAK\n6aIProXCBs+KwXzw2h7ATQocfthzoVDyNQYIefueHPV2kMNno2CxCNBIzaD7Bw5s\nu3xElijONlP2z0O/3ehhTb8iVnYIpS9iUi8Qghoji8WgKX0JQNSGWKPIIZCq9fzM\n41geDzFXAgMBAAECggEAB8EgZDxxAqwO6YseR4Lcyp2XM1yBWXfQI7sRMniUQLc8\nvazcm+DwTDcK9BB5KrJK3FwY1/v6OE2E7tbRtHFiZigOG1lnoV3pyOXOTuCgNxOD\nEI2hfSyOPvEPPcjk/8zjCvmXbBJ0Be+JSyXFt+tg4MNJwo2CwcVZ0tyU3s9IMPsZ\nT4NEDAmiz0FsR897XqehGkZvUum6eYN05zdFNXHw33hhbp5iq3Bctxf+CNzSGMV7\n/Zra7MPYXCI2L2YO0fM6zUKScyYnpv185xKIQVp6DR9kFULr249utzJq3pDyJ3VL\n9A4Gd2HMhDGSNHgRF9OBIxDE8z2/3CRozP4P0rdEmQKBgQDxvevtjn98+PINQ6Gt\nJcu1cmA2OFXsoQzgvtLiSpGk7sezwrTohgompzg7yR9LSvwevAyMUoRRrrTYkNyp\npsIdhlazvnAMXpDrLlTKLw9e62iJME82KQcnQgqg5uKoCkqHS3GuolKE1nOEhGPK\nBvZmOQ+juFqMZ+cigGQslsquqwKBgQC1D6la+b437ol+OG8BXV8/EqHN1AUBIaHf\nwKIf2wFKPRqvWMm4Skg3/n43EcGnfdU36sCWrq/ydNrzci95U2D0HeqqNzqhOxFZ\nT/Zivd8mP8/0zpEupkJrOjVrB+C5B535E5PGRgcN93KYubxSAn9nC/rS31eWZrEQ\njaZYZC1YBQKBgQCCWF6C+6fAMdcJ0eK2IsABOQeplJy392qjMCEzRPPdE6b4RU8Y\nZVXJ27ZVfi9ygJ8Kz2iQrNmN1X7LmuhwTWszUkEjr9ZoxQCs3pF3ZwKJsrLt7e94\nC41A3LowYe3qn4nqA4Lrn7iQybUFygCoaTKokbHeHEQumsOk9ceNx0zH+wKBgF4L\n0YLQwC4bN82ZEIeb8VI4olTgMO1Cg+tOCqgTQJtIG+lCbBzOcK6tAPAnx/fw02Rl\nCj36ZKfCbMwQ3nndhjmmpHJfl5ORs9Q5RZhKWXNrp9/Xv++EKnG53W9Hu0FApJxw\nv8w4KYfmpN6RczEB3R0wSstneP5FPumDOgkll6vlAoGBAMPakXEejITjNomKaUBX\ngIxfTTBWuWoE3q78zLnmDUMBOaLX4e5b7Xi0e0do30MUChM+dxF6aDLQl0ycBW+Q\nCu5PXTkuKOjc9bLlqVX/vmbRiA3ebVDQaCoVw3ED+36g9tyFA3JMmNCo46nA6oiv\nOynT9HetkK43NU9HmFLB69QH\n-----END PRIVATE KEY-----\n",
+            "client_email": "firebase-adminsdk-q1cj9@jobfast-359da.iam.gserviceaccount.com",
+            "client_id": "117389239828615707572",
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://accounts.google.com/o/oauth2/token",
+            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-q1cj9%40jobfast-359da.iam.gserviceaccount.com"
+        }
+    }
+}
+
+var jobochat = firebase.initializeApp({
+    credential: firebase.credential.cert(FIRE_BASE_ADMIN['jobochat'].cert),
+    databaseURL: FIRE_BASE_ADMIN['jobochat'].databaseURL
+}, "jobochat");
+var db = jobochat.database();
+var conversationData;
+
+db.ref('conversion').on('value', function (snap) {
+    conversationData = snap.val()
+})
 
 if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
     console.error("Missing config values");
@@ -98,24 +153,38 @@ app.post('/webhook', function (req, res) {
             var pageID = pageEntry.id;
             var timeOfEvent = pageEntry.time;
 
-            console.log('pageEntry', pageEntry.messaging)
             // Iterate over each messaging event
             pageEntry.messaging.forEach(function (messagingEvent) {
-                if (messagingEvent.optin) {
-                    receivedAuthentication(messagingEvent);
-                } else if (messagingEvent.message) {
-                    receivedMessage(messagingEvent);
-                } else if (messagingEvent.delivery) {
-                    receivedDeliveryConfirmation(messagingEvent);
-                } else if (messagingEvent.postback) {
-                    receivedPostback(messagingEvent);
-                } else if (messagingEvent.read) {
-                    receivedMessageRead(messagingEvent);
-                } else if (messagingEvent.account_linking) {
-                    receivedAccountLink(messagingEvent);
-                } else {
-                    console.log("Webhook received unknown messagingEvent: ", messagingEvent);
-                }
+
+                console.log('pageEntry', messagingEvent)
+
+                var savedMess = Object({}, messagingEvent)
+                delete savedMess.recipient
+                delete savedMess.sender
+                savedMess.messengerId = messagingEvent.sender.id
+                savedMess.type = 'received'
+                db.ref('conversion').child(savedMess.messengerId).child(savedMess.timestamp).update(savedMess).then(() => {
+
+                    if (messagingEvent.optin) {
+                        receivedAuthentication(messagingEvent);
+                    } else if (messagingEvent.message) {
+                        receivedMessage(messagingEvent);
+                    } else if (messagingEvent.delivery) {
+                        receivedDeliveryConfirmation(messagingEvent);
+                    } else if (messagingEvent.postback) {
+                        receivedPostback(messagingEvent);
+                    } else if (messagingEvent.read) {
+                        receivedMessageRead(messagingEvent);
+                    } else if (messagingEvent.account_linking) {
+                        receivedAccountLink(messagingEvent);
+                    } else {
+                        console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+                    }
+
+
+                })
+
+
             });
         });
 
@@ -223,6 +292,33 @@ function receivedAuthentication(event) {
  * then we'll simply confirm that we've received the attachment.
  * 
  */
+function jobJD({jobName = '', salary = '', hourly_wages = '', working_type = '', work_time = '', figure = '', unit = '', experience = '', sex = '', description = ''}, {storeData = {}, address = ''}) {
+    if (salary) salary = `🏆Lương: ${salary} triệu/tháng\n`;
+    if (hourly_wages) hourly_wages = `🏆Lương: ${hourly_wages} k/h + thưởng hấp dẫn\n`;
+    if (working_type) working_type = `🏆Hình thức làm việc: ${working_type}\n`;
+    let timeStr = '';
+    if (work_time) {
+        if (work_time.length > 1) {
+            timeStr = 'Ca làm:\n';
+            work_time.forEach(t => timeStr += `- ${t.start} giờ đến ${t.end} giờ\n`);
+        } else timeStr = `Ca làm: ${work_time[0].start} giờ - ${work_time[0].end} giờ`;
+    }
+
+    if (description) description = `🏆Mô tả công việc: ${description}\n`;
+    if (unit) unit = `🏆Số lượng cần tuyển: ${unit} ứng viên\n`;
+    if (experience) experience = `🏆Yêu cầu kinh nghiệm\n`;
+    else experience = '🏆Không cần kinh nghiệm\n';
+    if (sex === 'female') sex = `🏆Giới tính: Nữ\n`;
+    else if (sex === 'male') sex = `🏆Giới tính: Nam\n`;
+    if (figure) figure = '🏆Yêu cầu ngoại hình\n';
+    else figure = '🏆Không yêu cầu ngoại hình\n';
+
+    const text = `${storeName} - ${address}👩‍💻👨‍💻\n
+🏆Vị trí của bạn sẽ là: ${jobName}\n
+${working_type}${salary}${hourly_wages}${timeStr}\n${experience}${sex}${unit}${figure}${description}\n`
+    return text;
+}
+
 function receivedMessage(event) {
     var senderID = event.sender.id;
     var recipientID = event.recipient.id;
@@ -252,6 +348,54 @@ function receivedMessage(event) {
         var quickReplyPayload = quickReply.payload;
         console.log("Quick reply for message %s with payload %s",
             messageId, quickReplyPayload);
+        //vd: 'quickReply_confirmJob_yes_jobId'
+        var payload = quickReplyPayload.split('_');
+
+
+        if (payload[1] == 'confirmJob') {
+
+            if (payload[2] == 'yes') {
+                var jobId = payload[3];
+                loadJob(jobId).then(result => {
+                    var jobData = result
+
+                    var text = jobJD(jobData, jobData.storeData);
+
+                    var messageData = {
+                        recipient: {
+                            id: senderID
+                        },
+                        message: {
+                            text: `Hãy kiểm tra lại chi tiết công việc 1 lần nữa trước khi đặt lịch phỏng vấn nhé! \n ${text}`,
+                            quick_replies: [
+                                {
+                                    "content_type": "text",
+                                    "title": "Ứng tuyển",
+                                    "payload": "quickReply_bookingInterview_yes_" + jobId
+                                },
+                                {
+                                    "content_type": "text",
+                                    "title": "Từ chối ",
+                                    "payload": "quickReply_bookingInterview_no_" + jobId
+                                }
+                            ]
+                        }
+                    };
+
+                    callSendAPI(messageData);
+
+
+                })
+
+
+            } else {
+
+
+            }
+
+
+        }
+
 
         sendTextMessage(senderID, "Quick reply tapped");
         return;
@@ -373,44 +517,51 @@ function receivedPostback(event) {
     if (postback.referral) {
         var jobId = postback.referral.ref;
         // get data job
-        const url = `https://jobo-server.herokuapp.com/on/job?jobId=${jobId}`;
-        axios.get(url)
-            .then(res => {
-                var jobData = res.data;
 
-                var messageData = {
-                    recipient: {
-                        id: senderID
-                    },
-                    message: {
-                        text: `Có phải bạn đang muốn ứng tuyển vào vị trí ${jobData.jobName} của ${jobData.storeData.storeName} ?`,
-                        quick_replies: [
-                            {
-                                "content_type": "text",
-                                "title": "Đúng rồi (Y)",
-                                "payload": "DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_YES"
-                            },
-                            {
-                                "content_type": "text",
-                                "title": "Không phải",
-                                "payload": "DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_NO"
-                            },
-                        ]
-                    }
-                };
+        loadJob(jobId).then(result => {
+            var jobData = result
+            var messageData = {
+                recipient: {
+                    id: senderID
+                },
+                message: {
+                    text: `Có phải bạn đang muốn ứng tuyển vào vị trí ${jobData.jobName} của ${jobData.storeData.storeName} ?`,
+                    quick_replies: [
+                        {
+                            "content_type": "text",
+                            "title": "Đúng rồi (Y)",
+                            "payload": "quickReply_confirmJob_yes_" + jobId
+                        },
+                        {
+                            "content_type": "text",
+                            "title": "Không phải",
+                            "payload": "quickReply_confirmJob_no_" + jobId
+                        },
+                    ]
+                }
+            };
 
-                callSendAPI(messageData);
-            })
-            .catch(err => {
-                console.log(err);
-            });
+            callSendAPI(messageData);
+        }).catch(err => sendTextMessage(senderID, "Postback called"))
+
+
         //
-    } else  sendTextMessage(senderID, "Postback called");
+    } else sendTextMessage(senderID, "Postback called");
 
 
     // When a postback is called, we'll send a message back to the sender to
     // let them know it was successful
 }
+
+function loadJob(jobId) {
+    return new Promise(function (resolve, reject) {
+        const url = `https://jobo-server.herokuapp.com/on/job?jobId=${jobId}`;
+        axios.get(url)
+            .then(result => resolve(res.data))
+            .catch(err => reject(err));
+    })
+}
+
 
 /*
  * Message Read Event
@@ -867,6 +1018,7 @@ function callSendAPI(messageData) {
                 console.log("Successfully called Send API for recipient %s",
                     recipientId);
             }
+
         } else {
             console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
         }
