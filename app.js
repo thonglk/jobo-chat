6cab87c9eb1b4dbe4ed03d206a416528d07275d3
System Administrator <root@Admins-MacBook-Air.local>
2017-10-24 01:51:05 +0700

    first commit

--- app.js
+++ app.js
@@ -405,11 +405,11 @@ function receivedPostback(event) {
                 console.log(err);
             });
         //
-    }
+    } else  sendTextMessage(senderID, "Postback called");
+
 
     // When a postback is called, we'll send a message back to the sender to
     // let them know it was successful
-    sendTextMessage(senderID, "Postback called");
 }
 
 /*
