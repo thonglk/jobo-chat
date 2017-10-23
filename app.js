dba28b339aec47684730b0e30e0336c8b975261d
System Administrator <root@Admins-MacBook-Air.local>
2017-10-24 00:50:30 +0700

    first commit

--- app.js
+++ app.js
@@ -357,6 +357,7 @@ function receivedPostback(event) {
   // The 'payload' param is a developer-defined field which is set in a postback 
   // button for Structured Messages. 
   var payload = event.postback.payload;
+  console.log('Received',event)
 
   console.log("Received postback for user %d and page %d with payload '%s' " + 
     "at %d", senderID, recipientID, payload, timeOfPostback);
