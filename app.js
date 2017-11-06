45c06c038dc15f796bf104880f6996209ed741b8
System Administrator <root@Admins-MacBook-Air.local>
2017-11-07 02:18:41 +0700

    new

--- app.js
+++ app.js
@@ -1372,20 +1372,18 @@ app.post('/webhook', function (req, res) {
                     }
 
 
-                }
-
+                })
 
             }
-        );
-    }
-});
+        });
+
 
 // Assume all went well.
 //
 // You must send back a 200, within 20 seconds, to let us know you've
 // successfully received the callback. Otherwise, the request will time out.
-res.sendStatus(200);
-}
+        res.sendStatus(200);
+    }
 })
 ;
 
